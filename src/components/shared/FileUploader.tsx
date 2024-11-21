8f22daced7f73ba60659bc1ee88db210df63c4ae
Kunal Verma <96987213+DoaremonDev@users.noreply.github.com>
2024-11-21 12:53:02 +0530

    Update FileUploader.tsx

--- src/components/shared/FileUploader.tsx
+++ src/components/shared/FileUploader.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import  { useCallback, useState } from "react";
 import { FileWithPath, useDropzone } from "react-dropzone";
 import { Button } from "../ui/button";
 
@@ -10,6 +10,8 @@ type FileUploaderProps = {
 const FileUploader = ({ fieldChange, mediaUrl }: FileUploaderProps) => {
   const [file, setFile] = useState<File[]>([]);
   const [fileUrl, setFileUrl] = useState(mediaUrl);
+
+    console.log(file);
 
   const onDrop = useCallback(
     (acceptedFiles: FileWithPath[]) => {
